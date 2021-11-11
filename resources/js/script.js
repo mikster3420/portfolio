de37f89b470ac43340363c38a6cb8e5b4be58240
Mike <92549052+mikster3420@users.noreply.github.com>
2021-11-11 08:14:01 +0100

    Add index.html and style it. Also add JavaScript

---
+++ resources/js/script.js
@@ -0,0 +1,17 @@
+// // Global scope
+
+
+
+// // functions
+// function callName(message, element) {
+// 	const name = prompt("What's your name?");
+
+// 	const callName = document.querySelector(element);      
+// 	callName.innerHTML = `${message}, ${name}!`;
+
+// 	if (name === '') {
+// 		alert("You haven't typed your name. Please do.")
+// 	} 
+// }
+
+// callName("Welcome back", ".name");
